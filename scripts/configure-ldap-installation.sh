7faa5f42e76ce97ec79882fbb699d97e1352afdd
Lian Ulloa <l.ulloa@estudiantes.matcom.uh.cu>
2019-02-20 11:57:50 -0500

    Add Dockerfile for ldap server (without client)

---
+++ scripts/configure-ldap-installation.sh
@@ -0,0 +1,33 @@
+set timeout -1
+
+spawn apt -y install slapd ldap-utils
+expect "Administrator password: "
+send "$env(SERVER_PASSWORD)\r"
+expect "Confirm password: "
+send "$env(SERVER_PASSWORD)\r"
+sleep 10
+
+spawn dpkg-reconfigure slapd
+expect "Omit OpenLDAP server configuration?"
+send "No\r"
+expect "DNS domain name: "
+send "$env(SERVER_DOMAIN)\r"
+expect "Organization name: "
+send "MATCOM\r"
+expect "Administrator password: "
+send "$env(SERVER_PASSWORD)\r"
+expect "Confirm password: "
+send "$env(SERVER_PASSWORD)\r"
+expect "Database backend to use: "
+send "3\r"
+expect "Do you want the database to be removed when slapd is purged? "
+send "no\r"
+expect "Move old database? "
+send "yes\r"
+expect "Allow LDAPv2 protocol? "
+send "no\r"
+sleep 2
+
+# spawn apt install -y 
+interact 
+
