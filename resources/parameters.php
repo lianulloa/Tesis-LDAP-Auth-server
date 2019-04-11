ee0825627da348e2922ed2e8d083037f3bc1e80a
Lian Ulloa <l.ulloa@estudiantes.matcom.uh.cu>
2019-04-11 15:00:30 -0400

    Add sigenu's database credentials

---
+++ resources/parameters.php
@@ -0,0 +1,20 @@
+<?php
+define("DBHOST", "10.6.100.85");
+define("DBPORT", 5432);
+define("DBNAME","uh");
+define("DBUSER", "sigenu");
+define("DBPASSWORD", "d1run1c0*");
+
+define("MYSQL_DBHOST", "localhost");
+//define("MYSQL_DBPORT", 5432);
+define("MYSQL_DBNAME","mapeo_areas_sigenu_asset");
+define("MYSQL_DBUSER", "dirunico");
+define("MYSQL_DBPASSWORD", "d1run1c0*");
+
+//$host="localhost";
+//$port="5432";
+//$dbname="sigenu";
+//$user="postgres";
+//$password="d1run1c0*";
+
+?>
